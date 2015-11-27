00ec398586b0b5c0d9ab23760a676fb931dd3ef9
Spencer Krum <nibz@cat.pdx.edu>
2015-11-27 10:43:13 -0800

    Ensure gem utility is on the host
    The gem utility is needed by the archive module to install the faraday
    gem so that archive works.
    
    Change-Id: I914a6c6490e17ba02b4a6af4ed20c9f33931a33f

---
+++ manifests/params.pp
@@ -0,0 +1,22 @@
+# Params class
+class elasticsearch::params (
+){
+
+
+  case $::osfamily {
+    'Debian': {
+      if $::lsbdistcodename == 'precise' {
+        # package names
+        $gem_package = 'rubygems'
+      } else {
+        # package names
+        $gem_package = 'ruby'
+      }
+    }
+    default: {
+      $gem_package = 'ruby'
+    }
+  }
+
+}
+
